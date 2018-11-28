c874eb119f2763e6e0a874492817155618ab8c71
Silverdiscs <silverdiscs521@gmail.com>
2018-11-28 20:38:15 +0800

    提交 Day01 2018年11月28日 代码

---
+++ Test/test.java
@@ -0,0 +1,42 @@
+public class test 
+{
+    public static void main(String[] args)
+    {
+        // int num = 10;
+        // System.out.println(num * 2);
+        // int maxValue = Integer.MAX_VALUE;
+        // int minValue = Integer.MIN_VALUE;
+        // long num1 = (long)maxValue + 1;
+        // long num2 = (long)minValue - 1;
+        // System.out.println(maxValue + 1L);
+        // System.out.println(minValue - 1L);
+        // byte b1 = (byte)300;
+        // System.out.println(b1);
+
+        // double d1 = 10.2;  //10.2为浮点数常量,默认为double类型
+        // System.out.print(d1 * 2);
+        // float f1 = 1.1F;
+        // float f2 = 1.1F;
+        // System.out.println(f1 * f2);
+        // System.out.print(1.1 * 1.1); //IEEE64会造成精度的丢失（如生成1.2100000000000002后面的002），单精度浮点数不会出现这个问题
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
