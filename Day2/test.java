cd9bd154c2303891c3b7ad5015e4ffd4cb3c666a
Silverdiscs <silverdiscs521@gmail.com>
2018-11-29 21:32:17 +0800

    运算符、Java逻辑控制与方法定义（循环）

---
+++ Day2/test.java
@@ -0,0 +1,44 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        //字母的大小写转换
+        // char c = 'A';
+        // int a = c + 32;
+        // char result = (char)a;
+        // System.out.print(result);
+
+        //测试字符0 和 int 0 相不相等
+        // if('0' == 0)
+        // {
+        //     System.out.print("相等");
+        // }
+        // else
+        // {
+        //     System.out.print("不相等");
+        // }
+
+        // String的使用
+        // String str1 = "临潼Java7班";
+        // String str2 = "开课了！";
+        // System.out.println(str1 + str2);
+
+
+        // int num1 = 10;
+        // int num2 = 20;
+        // // System.out.println("计算结果为："+num1+num2);
+        // System.out.println("计算结果为："+(num1+num2));
+
+
+        //三目运算符的使用
+        // int x = 3;
+        // int y = 4;
+        // int result = x - y > 0 ? x + y : x - y;
+        // System.out.print(result);
+
+        //左移，右移(笔试题)
+        // System.out.println(2>>4);
+        // System.out.println(2<<4);
+
+    }
+}
---
+++ Day2/test01.java
@@ -0,0 +1,45 @@
+public class test01
+{
+    public static void main(String[] args) throws Exception
+    {
+        //输出等边三角形
+        // int line = 10;
+        // for(int x = 0; x < line; x++)
+        // {
+        //     for(int y = 0; y < line - x; y++)
+        //     {
+        //         System.out.print(" ");
+        //     }
+        //     for(int z = 0; z < x; z++)
+        //     {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // //switch语句的使用
+        // System.out.println("请输入字符...");
+        // char c = (char) System.in.read();
+        // switch(c)
+        // {
+        //     case 'a':
+        //     {
+        //         System.out.println("大屁屁");
+        //         break;
+        //     }
+        //     case 'b':
+        //     {
+        //         System.out.println("大胖人");
+        //         break;
+        //     }
+        //     default:
+        //         System.out.println("大胖菡");
+        //         break;
+        // }
+
+
+        
+
+    }  
+}
+
